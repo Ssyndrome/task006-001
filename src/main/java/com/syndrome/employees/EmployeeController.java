0f54d3d143ece30a53d18450bc9fe106df7a9e0d
915448715@qq.com <915448715@qq.com>
2018-04-28 20:50:40 +0800

    add thym

--- src/main/java/com/syndrome/employees/EmployeeController.java
+++ src/main/java/com/syndrome/employees/EmployeeController.java
@@ -5,22 +5,28 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class EmployeeController {
 
     static Map<Long, Employee> employees = Collections.synchronizedMap(new HashMap<Long, Employee>());
 
+    @ResponseBody
     @GetMapping(value = "/employees")
     public List<Employee> getEmployees () {
         List<Employee> result = new ArrayList<Employee>(employees.values());
         return result;
     }
 
+    @ResponseBody
     @PostMapping(value = "/employees")
     public String addEmployee ( @RequestParam("id") Long id,
                                 @RequestParam("name") String name,
@@ -34,5 +40,14 @@ public class EmployeeController {
 
         employees.put(id,employee);
         return "success";
+    }
+
+    @RequestMapping("/")
+    public ModelAndView index(){
+        List<Employee> userList =new ArrayList<Employee>(employees.values());
+
+        ModelAndView modelAndView = new ModelAndView("/index");
+        modelAndView.addObject("userList", userList);
+        return modelAndView;
     }
 }
